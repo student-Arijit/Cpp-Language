b3401970889e1065f3f128ae3c128f2cb810e23d
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2026-08-02 20:32:30 +0530

    Add program to print square star pattern
    This program prints a square star pattern of asterisks based on the specified size.

---
+++ Loops in CPP/03 Pattern Problems/Star Patterns/01 Square Star Pattern.cpp
@@ -0,0 +1,16 @@
+//Print a Square Star Pattern
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n = 5;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
